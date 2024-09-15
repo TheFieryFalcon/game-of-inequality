bb700aa0c521dab3fbff9013ce17b6007e4a27c9
Rolenzo9194 <148025034+Rolenzo9194@users.noreply.github.com>
2024-09-15 21:57:12 +0800

    Which is better
    This or your one? Or shall we combine them and keep all?

---
+++ initialisecards.py
@@ -0,0 +1,17 @@
+def initialisecards():
+    main.Card(main.CardType.PROPERTY, "Double Lane", 1, b=55, c=main.Set.BROWN)
+    main.Card(main.CardType.PROPERTY, "Euston Road", 2, b=70, c=main.Set.BROWN)
+    main.Card(main.CardType.PROPERTY, "Riverfield Drive", 3, b=120, c=main.Set.BROWN)
+    main.Card(main.CardType.PROPERTY, "Goldington Road", 4, b=60, c=main.Set.BROWN)
+    main.Card(main.CardType.PROPERTY, "Pinetree Gully Road", 5, b=40, c=main.Set.RED)
+    main.Card(main.CardType.PROPERTY, "Acanthus Road", 6, b=180, c=main.Set.RED)
+    main.Card(main.CardType.PROPERTY, "Riverton Forum", 7, b=250, c=main.Set.RED)
+    main.Card(main.CardType.PROPERTY, "Murray Street", 8, b=350, c=main.Set.RED)
+    main.Card(main.CardType.PROPERTY, "Woolworths Way", 9, b=100, c=main.Set.BLUE)
+    main.Card(main.CardType.PROPERTY, "Hacker Way", 10, b=200, c=main.Set.BLUE)
+    main.Card(main.CardType.PROPERTY, "Ampitheatre Parkway", 11, b=300, c=main.Set.BLUE)
+    main.Card(main.CardType.PROPERTY, "Castle Donington", 12, b=400, c=main.Set.BLUE)
+    return Card
+    board = [go, mave, chest, bave, itax, rrail, oave, chance, vave, cave, jvail, scpl, elec, save, viave, ]
+
+# which is better this or yours
